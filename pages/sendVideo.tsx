e859475cd97443475b7031aa5c20a2f9c4e89a46
木村愛哉 <kimuramanaya@kimulocal.local>
2023-06-19 23:06:57 +0900

    input要素の動画データの名前を表示するように変更

--- pages/sendVideo.tsx
+++ pages/sendVideo.tsx
@@ -6,15 +6,17 @@ import { useRecoilValue } from 'recoil';
 import { isLogginState } from 'atoms/isLogginState';
 import { userIdState } from "atoms/userIdState";
 import { userEmailState } from "atoms/userEmailState";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 
 const SendVideoPage = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, watch } = useForm();
     const isLoggin = useRecoilValue(isLogginState);
     const userId = useRecoilValue(userIdState);
     const userEmail = useRecoilValue(userEmailState);
+    const [videoName, setVideoName] = useState("")
+    const inputVideoData = watch("video")
 
     const onSubmit = async (data: any) => {
         if (isLoggin) {
@@ -35,18 +37,25 @@ const SendVideoPage = () => {
         console.log(isLoggin)
     }, [])
 
+    useEffect(() => {
+        if (inputVideoData) {
+            setVideoName(inputVideoData[0].name)
+        }
+    }, [inputVideoData])
+
+
 
 
     return (<>
         <div className={styles.container}>
             <Image className={styles.image} src="/images/sendVideo.jpg" alt="Image" width={1000} height={800} />
+            <h1 className={styles.header}>Upload</h1>
             <div className={styles.overlay} >
                 {isLoggin ? (
                     <>
-                        <h1 className={styles.title}>アップロード</h1>
                         <form className={styles.form} onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
                             <p>
-                                ファイルを選択してください
+                                {videoName ? (videoName + "が選択されています") : ("ファイルが選択されていません")}
                             </p>
                             <label>
                                 <input type="file" className={styles.input}  {...register("video")} />ファイルを選択
