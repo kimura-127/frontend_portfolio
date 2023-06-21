9e2be8dce9f342c3f47a670cceb26868e4783e0d
木村愛哉 <kimuramanaya@kimulocal.local>
2023-06-21 22:05:29 +0900

    インプット要素にバリデーション機能を追加

--- pages/sendVideo.tsx
+++ pages/sendVideo.tsx
@@ -13,20 +13,29 @@ import { useRouter } from "next/router";
 
 const SendVideoPage = () => {
     // フォームで使用する関数
-    const { register, handleSubmit, watch } = useForm();
+    const { register, handleSubmit, watch, reset } = useForm();
     const inputVideoData = watch("video")
     // Recoil宣言
     const isLoggin = useRecoilValue(isLogginState);
     const userId = useRecoilValue(userIdState);
     const userEmail = useRecoilValue(userEmailState);
     // フォームを選択したら動画の名前を表示するため使用
-    const [videoName, setVideoName] = useState("")
+    const [videoName, setVideoName] = useState("");
+    // inputのデータが動画か否かの状態を保持
+    const [inputDataValue, setInputDataValue] = useState(true);
     // useRouterを宣言
     const router = useRouter();
+    // フォームの中身をリセットする関数
+    const formReset = () => {
+        reset();
+    }
+    // データに異常がある状態でアップロードを押した時に使う
+    const [missingAppload, setMissingAppload] = useState(false)
 
     // アップロードボタンをクリックした時の処理
     const onSubmit = async (data: any) => {
-        if (isLoggin) {
+        // ログインしていて尚且つインプットのデータに異常がなければ処理開始
+        if (isLoggin && inputDataValue && videoName.length > 0) {
             const formData = new FormData();
             // 動画データをアペンド
             formData.append("video", data.video[0]);
@@ -39,6 +48,8 @@ const SendVideoPage = () => {
                 }
             }
             )
+        } else {
+            setMissingAppload(true)
         }
     }
 
@@ -49,10 +60,23 @@ const SendVideoPage = () => {
         }
     }, [])
 
-    // フォームのinput要素が変更されたらvideoNameをセット
+    // inputの要素が動画かチェック && trueならデータ名を画面に表示
     useEffect(() => {
         if (inputVideoData && inputVideoData.length > 0) {
-            setVideoName(inputVideoData[0].name)
+            const videoDataName = inputVideoData[0].name
+            // データの拡張子を以下の内容で点検
+            const testInputData = /mov$|mp4$|avi$|wmv$|mkv$|flv$|webm$|m4v$|mpeg$|3gp$/;
+            // 動画であるかどうかチェック
+            const result = testInputData.test(videoDataName)
+            if (result) {
+                setInputDataValue(true)
+                setVideoName(videoDataName)
+            } else {
+                // falseならフォームリセット
+                formReset();
+                setInputDataValue(false)
+
+            }
         }
     }, [inputVideoData])
 
@@ -68,12 +92,14 @@ const SendVideoPage = () => {
                     <>
                         <form className={styles.form} onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
                             <p>
-                                {videoName ? (videoName + "が選択されています") : ("ファイルが選択されていません")}
+                                {inputDataValue ? (
+                                    videoName ? (videoName + "が選択されています") : ("ファイルが選択されていません")
+                                ) : ("動画以外はアップロードできません")}
                             </p>
                             <label>
                                 <input type="file" className={styles.input}  {...register("video")} />ファイルを選択
                             </label>
-                            <button type="submit" className={styles.button}>アップロード</button>
+                            <button type="submit" className={missingAppload ? (styles.missbutton) : (styles.button)}>アップロード</button>
                         </form>
                     </>
                 ) : (
